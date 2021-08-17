fa32f0926f8f8a9e6079ad0e2fcdaff9d16381c2
emMercado <mercadobemma55@gmail.com>
2021-08-17 10:19:08 -0300

    update

--- src/app/redux/actions/pointsActions.js
+++ src/app/redux/actions/pointsActions.js
@@ -4,14 +4,13 @@ import axios from 'axios';
 //Action Types
 export const SHOW_POINTS = 'SHOW_POINTS';
 
-export function getPointsAllPoints(){
-  
+export function getPointsAllPoints() {
+
   return async (dispatch, getState) => {
     await axios.get(routes.sample())
-    .then((response) => {
-             console.log(response)
-             dispatch ({ type: SHOW_POINTS, payload: response.data } )
-         }).catch
- }
-
-} 
+      .then((response) => {
+        console.log(response)
+        dispatch({ type: SHOW_POINTS, payload: response.data })
+      }).catch
+  }
+}
